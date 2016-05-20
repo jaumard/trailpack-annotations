fb4772902d3d63f9a6a99a0fa24bef5de251e150
jaumard <ioquoo3H>
2016-05-20 21:02:06 +0200

    Add globalContext options for parsing annotations

--- config/annotations.js
+++ config/annotations.js
@@ -1,6 +1,12 @@
+'use strict'
+
+const Joi = require('joi')
 module.exports = {
   policy: true,
   route: true,
+  globalContext: {
+    Joi: Joi
+  },
   pathToScan: './api/controllers',
   customAnnotations: require('../annotations')
 }
